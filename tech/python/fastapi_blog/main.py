8ca07c3ef64ae297a79ffbcb1bb1a3676af83b82
AyushSonuu <sonuayush55@gmail.com>
2026-06-17 22:37:39 +0530

    📝 update: fastapi_blog — add path parameter route GET /items/{item_id}

--- tech/python/fastapi_blog/main.py
+++ tech/python/fastapi_blog/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 app =FastAPI()
 
 @app.get("/")
-def root():
+def get_root():
     return {"message": "Hello World"}
 
 
+@app.get("/items/{item_id}")
+def get_item(item_id:int):
+    return {"item_id": item_id}
